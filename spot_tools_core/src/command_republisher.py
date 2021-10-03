426bc0b176c52c7248511bc05cbe137bad226245
Eric V <eric.vollenweider@gmail.com>
2021-10-04 00:06:33 +0200

    TDD publish command feedback on missing anchor commands

--- spot_tools_core/src/command_republisher.py
+++ spot_tools_core/src/command_republisher.py
@@ -1,4 +1,4 @@
-import rospy
+#!/usr/bin/env python3
 
 '''
 Type: Ros-Node
@@ -7,10 +7,84 @@ Input: Commands on the topic "/command/raw"
 Output: Commands on the topic "/command/processed"
 
 '''
-
+import rospy
+from geometry_msgs.msg import PoseStamped, TransformStamped
+import tf2_ros as tf
+from std_msgs.msg import String
 class CommandRepublisher():
     def __init__(self):
         rospy.init_node("command_republisher")
+
+        rospy.Subscriber("/command/raw", PoseStamped, self.received_cmd)
+        self.cmd_pub = rospy.Publisher("/command/processed", PoseStamped, queue_size=1)
+        self.cmd_feedback_pub = rospy.Publisher("/command/feedback", String, queue_size=10)
+
+        #tf setup
+        self.tf_Buffer = tf.Buffer(rospy.Duration(10))
+        self.tf_listener = tf.TransformListener(self.tf_Buffer)
+        self.tf_broadcaster = tf.TransformBroadcaster()
+        self.tf_static_broadcaster = tf.StaticTransformBroadcaster()
+
+        self.robot_frame = "body"
+        self.goal_frame_id = "current_goal"
+
+
+
+    def received_cmd(self, cmd):
+        rospy.loginfo("received command!")
+        self.cmd_feedback_pub.publish("Received command")
+
+        # Lookup cmd relative to body frame
+        _tf = self.pose_to_tf(cmd.pose, self.goal_frame_id, cmd.header.frame_id)
+        self.tf_broadcaster.sendTransform(_tf)
+
+        rospy.sleep(0.1)
+
+        # lookup transform to odom
+        try:
+            trans = self.tf_Buffer.lookup_transform(self.robot_frame, self.goal_frame_id, rospy.Time(0), rospy.Duration(1))
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            rospy.loginfo("Failed to lookup anchor for anchored goal once.")
+            self.cmd_feedback_pub.publish("Fail; Could not find anchor")
+            return
+
+        
+        target_pose = PoseStamped()
+        target_pose.header.frame_id = self.robot_frame
+        target_pose.pose.position.x = trans.transform.translation.x
+        target_pose.pose.position.y = trans.transform.translation.y
+        target_pose.pose.position.z = trans.transform.translation.z
+        target_pose.pose.orientation.x = trans.transform.rotation.x
+        target_pose.pose.orientation.y = trans.transform.rotation.y
+        target_pose.pose.orientation.z = trans.transform.rotation.z
+        target_pose.pose.orientation.w = trans.transform.rotation.w
+
+        self.cmd_pub.publish(target_pose)
+        self.cmd_feedback_pub.publish("Success; Republished command")
+
+
+
+    # transforms a post to a transform object
+    def pose_to_tf(self, pose, frame_name, parent_frame, time=None):
+        """
+        Generate a TF from a given pose, frame, and parent.
+        """
+        assert pose is not None, 'Cannot have None for pose.'
+        _tf = TransformStamped()
+        
+        _tf.child_frame_id = frame_name
+        if time is None:
+            time = rospy.Time.now()
+        _tf.header.stamp = time
+        _tf.header.frame_id = parent_frame
+
+        _tf.transform.translation = pose.position
+        _tf.transform.rotation = pose.orientation
+
+        return _tf
+
+    def spin(self):
+        rospy.spin()
 
 if __name__ == "__main__":
     c = CommandRepublisher()
--- spot_tools_core_tests/src/command_republisher_tests.py
+++ spot_tools_core_tests/src/command_republisher_tests.py
@@ -1,20 +1,48 @@
+#!/usr/bin/env python3
+
 import rospy
 import sys
-# sys.path.insert(0, '/home/eric/catkin_ws/src/spot-mr-core/spot_tools_core_tests')
 
 from spot_tools_tests_common.test_class import TestClass
 from asa_ros_msgs.msg import FoundAnchor
 import tf2_ros as tf
 from geometry_msgs.msg import TransformStamped, PoseStamped
 from spot_tools_tests_common.asa_mock_utils import get_mock_tf_frame
-
+from std_msgs.msg import String
 class CommandRepublisherTests(TestClass):
     def __init__(self):
         super(CommandRepublisherTests, self).__init__('command_republisher_tests', True)
 
         self.command_pub = rospy.Publisher('/spot/command/pose', PoseStamped, queue_size=10)
         rospy.Subscriber('/spot/command/processed', PoseStamped, self.received_processed_cmd)
+        rospy.Subscriber('/spot/command/feedback', String, self.command_feedback_received)
+        self.register_test_handle("MissingAnchorCmd", "Should receive failed cmd feedback", self.test_missin_anchor_cmd)
         self.register_test_handle("RepublishCmdRelative", "Command was not republished correctly", self.test_republish_cmd)
+
+        rospy.sleep(1)
+
+##########
+# TEST 0 #
+##########
+
+    def test_missin_anchor_cmd(self):
+
+        # publish a command
+        cmd = PoseStamped()
+        cmd.header.frame_id = "non_existing_anchor_id_rot"
+        cmd.pose.position.x = 0
+        cmd.pose.position.y = 1
+        cmd.pose.orientation.w = 1
+        self.command_pub.publish(cmd)
+
+    def command_feedback_received(self, data):
+        if self.test_idx == 0:
+            if data.data == "Fail; Could not find anchor":
+                self.advance_test(0, True)
+
+##########
+# TEST 1 #
+##########
 
     # validates that the command republisher node correctly processes commands
     # it should take the command from anchor -> target pose, and re-calculate the command from 
@@ -41,8 +69,6 @@ class CommandRepublisherTests(TestClass):
         t.child_frame_id = "test_anchor_rot"
         self.tf_static_broadcaster.sendTransform(t)
 
-        
-
         # mock robot body base
         body = get_mock_tf_frame()
         body.header.frame_id = "odom"
@@ -58,11 +84,14 @@ class CommandRepublisherTests(TestClass):
         cmd.header.frame_id = "test_anchor_rot"
         cmd.pose.position.x = 0
         cmd.pose.position.y = 1
+        cmd.pose.orientation.w = 1
         self.command_pub.publish(cmd)
 
+        rospy.loginfo("Published command!")
+
     def received_processed_cmd(self, data):
-        success = data.pose.x == 1 and data.pose.y == 0
-        self.advance_test(0, success)
+        success = data.pose.position.x == 1 and data.pose.position.y == 0
+        self.advance_test(1, success)
 
 if __name__ == "__main__":
     tester = CommandRepublisherTests()
